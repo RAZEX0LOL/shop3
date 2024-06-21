45d9007feddef362cecd7eece90955e0a6cecc74
RAZEX0LOL <99183060+RAZEX0LOL@users.noreply.github.com>
2024-06-21 21:24:53 +0400

    add orders, icons, styles and logic

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
+import { IoLogoGitlab } from "react-icons/io5";
+import { IoMdBasket } from "react-icons/io";
+import logo from "../../assets/icon.png"
 
 export default function Header(){
+    let [cartOpen,setCartOpen]=useState(false);
+
     return(
         <header>
             <div>
+                {/* <IoLogoGitlab className={styles.icon}/> */}
+                <img src={logo} alt="logo" />
                 <span className={styles.logo}>Games Shop</span>
                 <ul className={styles.nav}>
                     <li>О нас</li>
                     <li>Контакты</li>
                     <li>Личный кабинет</li>
                 </ul>
+                <IoMdBasket onClick={()=>setCartOpen(cartOpen= !cartOpen)} className={`${styles.shopCartButton} ${cartOpen ? styles.active : ''}`}/>
+
+                 {cartOpen &&(
+                     <div className={styles.shopCart}>
+
+                     </div>
+                 )}
             </div>
             <div className={styles.presentation}></div>
         </header>
