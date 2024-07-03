ba6084567c860af9551f6758ad313f78f053265a
RAZEX0LOL <99183060+RAZEX0LOL@users.noreply.github.com>
2024-07-03 20:04:41 +0400

    add pricelist download in pdf file

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,11 +2,21 @@ import React, { useState } from "react";
 import styles from "./Header.module.scss";
 import { IoLogoGitlab } from "react-icons/io5";
 import { IoMdBasket } from "react-icons/io";
-import logo from "../../assets/icon.png"
 import Orders from "../Orders";
+import logo from "../../assets/logo.png";
+import PriceListPDF from "../PriceListPDF";
+import { saveAs } from "file-saver";
+import { pdf } from "@react-pdf/renderer";
 
 export default function Header(props){
     let [cartOpen,setCartOpen]=useState(false);
+
+
+    const handleDownloadPDF = async () =>{
+        const pdfBlob =await pdf(<PriceListPDF items={props.items}/>).toBlob();
+        saveAs(pdfBlob, "PriceList.pdf");
+
+    }
 
     const showOrders=(props)=>{
         let summa=0;
@@ -35,12 +45,12 @@ export default function Header(props){
         <header>
             <div>
                 {/* <IoLogoGitlab className={styles.icon}/> */}
-                <img src={logo} alt="logo" />
+                <img width={60} height={60} src={logo} alt="Logotype" />
                 <span className={styles.logo}>Games Shop</span>
                 <ul className={styles.nav}>
                     <li>О нас</li>
                     <li>Контакты</li>
-                    <li>Личный кабинет</li>
+                    <li onClick={handleDownloadPDF}>Скачать прайс-лист</li>
                 </ul>
                 <IoMdBasket onClick={()=>setCartOpen(cartOpen= !cartOpen)} className={`${styles.shopCartButton} ${cartOpen ? styles.active : ''}`}/>
 
